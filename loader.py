eac91d0599ce999f8363098e6fb8070aff74a444
wildrun0 <gamesnouth@yandex.ru>
2023-05-24 16:19:38 +0500

    logger from loguru disabled

--- loader.py
+++ loader.py
@@ -1,6 +1,7 @@
 from vkbottle.bot import Bot
 from data.config import BOT_TOKEN
 from config import BotConfig
+from loguru import logger
 import logging
 
 FORMAT = '%(asctime)s [coolbot] %(message)s'
@@ -8,6 +9,8 @@ FORMAT = '%(asctime)s [coolbot] %(message)s'
 bot = Bot(token=BOT_TOKEN)
 BotConfig(bot).init()
 
+
+logger.disable("vkbottle")
 logging.basicConfig(encoding='utf-8', level=logging.INFO, format=FORMAT, handlers=[
     logging.FileHandler("coolbot.log"),
     logging.StreamHandler()
